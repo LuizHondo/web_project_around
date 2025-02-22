05893f51addcc68ce335a2896b44045ceca64ba1
LuizHondo <lpmoh2@hotmail.com>
2025-02-22 20:19:08 -0300

    Finished first project version

---
+++ scripts/script.js
@@ -0,0 +1,34 @@
+// Elements
+const popup = document.querySelector(".popup");
+const nameInput = document.querySelector("#name");
+const descriptionInput = document.querySelector("#description");
+const profileName = document.querySelector(".profile__name__text");
+const profileDescription = document.querySelector(".profile__description");
+const popupCloseButton = document.querySelector(".popup__close-button");
+const editButton = document.querySelector(".profile__name__edit-button-image");
+const saveProfileButton = document.querySelector(".popup__submit");
+const profileForm = document.querySelector(".popup__form");
+// Functions
+function getElements() {
+  nameInput.value = profileName.textContent;
+  descriptionInput.value = profileDescription.textContent;
+}
+function setElements(evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileDescription.textContent = descriptionInput.value;
+  closePopup();
+}
+function openPopup() {
+  popup.classList.remove("popup--hidden");
+  getElements();
+}
+function closePopup() {
+  popup.classList.add("popup--hidden");
+}
+getElements();
+
+// Event Listeners
+popupCloseButton.addEventListener("click", closePopup);
+editButton.addEventListener("click", openPopup);
+profileForm.addEventListener("submit", setElements);
